552811b929b88b8d2ec252739d15691628569a3f
Wande Tricada <wandetri@grms.jatengprov.go.id>
2020-11-01 16:42:52 +0700

    Add pm2 process

--- run.sh
+++ run.sh
@@ -1,13 +1,17 @@
 # Turn on maintenance mode
 php artisan down || true
 
-# Pull the latest changes from the git repository
-# git reset --hard
-# git clean -df
-# git pull origin master
+# Kill Socket.io Server & Queue process
+pm2 delete server.yaml
 
 # Install/update composer dependecies
 composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
+
+#Generate JWT Secret
+php artisan jwt:secret 
+
+#Generate Key
+php artisan key:generate 
 
 # Run database migrations
 php artisan migrate --force
@@ -15,23 +19,25 @@ php artisan migrate --force
 # Clear caches
 php artisan cache:clear
 
+#Clear Queue
+php artisan queue:flush
+
+#Restart Queue
+php artisan queue:restart
+
 # Clear expired password reset tokens
 php artisan auth:clear-resets
 
-# Clear and cache routes
-php artisan route:cache
+
+# Clear and cache config
+php artisan config:clear
 
 # Clear and cache config
 php artisan config:cache
 
 # Clear and cache views
 php artisan view:cache
-
-#Generate JWT Secret
-php artisan jwt:secret 
-
-#Generate Key
-php artisan key:generate        
+      
 
 # Install node modules
 npm ci
@@ -42,11 +48,8 @@ npm run dev
 # Turn off maintenance mode
 php artisan up
 
-#Start Laravel
-php artisan ser &
+# Start Socket.io Server & Queue
+pm2 start server.yaml
 
-#Start Queue
-php artisan queue:work &
-
-#Start Socket.io server
-node server.js &
+# Start Laravel
+php artisan serve
